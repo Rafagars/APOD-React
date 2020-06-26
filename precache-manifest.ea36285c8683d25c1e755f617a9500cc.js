c35b41dfd556c6b35b7e573d916f77675cf5f7cb
rafagar96@gmail.com <rafagar96@gmail.com>
2020-06-26 15:10:14 -0400

    Updates

---
+++ precache-manifest.ea36285c8683d25c1e755f617a9500cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "def8ec3caf1bc866ea4161e947570f8f",
+    "url": "/APOD-React/index.html"
+  },
+  {
+    "revision": "7d1223656d3a45d39f13",
+    "url": "/APOD-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "da41892e9a970e9813f9",
+    "url": "/APOD-React/static/css/main.36c35d6b.chunk.css"
+  },
+  {
+    "revision": "7d1223656d3a45d39f13",
+    "url": "/APOD-React/static/js/2.0c599fbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/APOD-React/static/js/2.0c599fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da41892e9a970e9813f9",
+    "url": "/APOD-React/static/js/main.bcabd081.chunk.js"
+  },
+  {
+    "revision": "88cb080e02f59846beb4",
+    "url": "/APOD-React/static/js/runtime-main.ffd1b17d.js"
+  },
+  {
+    "revision": "13928a8829bae43b4dc5bbf92097efd3",
+    "url": "/APOD-React/static/media/bg.13928a88.jpg"
+  }
+]);
