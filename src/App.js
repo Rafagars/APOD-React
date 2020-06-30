6d5ecd9dbe18175200de38e66f49870f24150d14
rafagars <rafagar96@gmail.com>
2020-06-29 20:51:26 -0400

    Added footer with credits to the original authors & editors, to avoid any misunderstanding

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router,} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
 	render(){
 		return(
 			<Router>
 				<Header />
+				<Footer />
 			</Router>
 			)
 	}
